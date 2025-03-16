491dca0f306e964bfd8682af70cbeb454918817f
roma1524 <kulikr.js@mail.ru>
2025-03-16 21:32:00 +0300

    deleteTask

--- Thunk/src/features/todolists/model/tasks-slice.ts
+++ Thunk/src/features/todolists/model/tasks-slice.ts
@@ -1,89 +1,121 @@
-import {createTodolistTC, deleteTodolistTC} from "./todolists-slice"
-import {createAppSlice} from "@/common/utils";
-import {tasksApi} from "@/features/todolists/api/tasksApi.ts";
-import {DomainTask} from "@/features/todolists/api/tasksApi.types.ts";
-import {TaskPriority, TaskStatus} from "@/common/enums";
-import {nanoid} from "@reduxjs/toolkit";
+import { createTodolistTC, deleteTodolistTC } from "./todolists-slice";
+import { createAppSlice } from "@/common/utils";
+import { tasksApi } from "@/features/todolists/api/tasksApi.ts";
+import { DomainTask } from "@/features/todolists/api/tasksApi.types.ts";
+import { TaskPriority, TaskStatus } from "@/common/enums";
+import { nanoid } from "@reduxjs/toolkit";
 
 export const tasksSlice = createAppSlice({
-    name: "tasks",
-    initialState: {} as TasksState,
-    selectors: {
-        selectTasks: (state) => state,
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(createTodolistTC.fulfilled, (state, action) => {
-                state[action.payload.todolist.id] = []
-            })
-            .addCase(deleteTodolistTC.fulfilled, (state, action) => {
-                delete state[action.payload.id]
-            })
-    },
-    reducers: (create) => ({
-        deleteTaskAC: create.reducer<{ todolistId: string; taskId: string }>((state, action) => {
-            const tasks = state[action.payload.todolistId]
-            const index = tasks.findIndex((task) => task.id === action.payload.taskId)
-            if (index !== -1) {
-                tasks.splice(index, 1)
-            }
-        }),
-        createTaskAC: create.reducer<{ todolistId: string; title: string }>((state, action) => {
-            const newTask: DomainTask = {title: action.payload.title,
-                status: TaskStatus.New,
-                id: nanoid(),
-                todoListId: action.payload.todolistId,
-                deadline: '',
-                order: 1,
-                priority: TaskPriority.Hi,
-                description: '',
-                startDate: '',
-                addedDate: ''
-            }
-            state[action.payload.todolistId].unshift(newTask)
-        }),
-        changeTaskStatusAC: create.reducer<{ todolistId: string; taskId: string; status: TaskStatus }>((state, action) => {
-            const task = state[action.payload.todolistId].find((task) => task.id === action.payload.taskId)
-            if (task) {
-                task.status = action.payload.status
-            }
-        }),
-        changeTaskTitleAC: create.reducer<{ todolistId: string; taskId: string; title: string }>((state, action) => {
-            const task = state[action.payload.todolistId].find((task) => task.id === action.payload.taskId)
-            if (task) {
-                task.title = action.payload.title
-            }
-        }),
-
-        // --------------  Thunk  --------------
-
-        fetchTasks: create.asyncThunk(async (todolisrId: string, {rejectWithValue}) => {
-            try {
-                const res = await tasksApi.getTasks(todolisrId)
-                return {tasks: res.data.items, todolisrId};
-            } catch (e) {
-                return rejectWithValue(null)
-            }
-        }, {
-            fulfilled: (state, action) => {
-                state[action.payload.todolisrId] = action.payload.tasks
-            }
-        }),
-        createTask: create.asyncThunk(async (args: { todolistId: string, title: string }, {rejectWithValue}) => {
-            try {
-                const res = await tasksApi.createTask(args)
-                return {task: res.data.data.item}
-            } catch (error) {
-                return rejectWithValue(error)
-            }
-        }, {fulfilled: (state, action) => {
-                state[action.payload.task.todoListId].unshift(action.payload.task)
-            }})
+  name: "tasks",
+  initialState: {} as TasksState,
+  selectors: {
+    selectTasks: (state) => state,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createTodolistTC.fulfilled, (state, action) => {
+        state[action.payload.todolist.id] = [];
+      })
+      .addCase(deleteTodolistTC.fulfilled, (state, action) => {
+        delete state[action.payload.id];
+      });
+  },
+  reducers: (create) => ({
+    changeTaskStatusAC: create.reducer<{
+      todolistId: string;
+      taskId: string;
+      status: TaskStatus;
+    }>((state, action) => {
+      const task = state[action.payload.todolistId].find(
+        (task) => task.id === action.payload.taskId,
+      );
+      if (task) {
+        task.status = action.payload.status;
+      }
     }),
-})
+    changeTaskTitleAC: create.reducer<{
+      todolistId: string;
+      taskId: string;
+      title: string;
+    }>((state, action) => {
+      const task = state[action.payload.todolistId].find(
+        (task) => task.id === action.payload.taskId,
+      );
+      if (task) {
+        task.title = action.payload.title;
+      }
+    }),
 
-export const {selectTasks} = tasksSlice.selectors
-export const {deleteTaskAC, createTaskAC, changeTaskStatusAC, changeTaskTitleAC, fetchTasks, createTask} = tasksSlice.actions
-export const tasksReducer = tasksSlice.reducer
+    // --------------  Thunk  --------------
 
-export type TasksState = Record<string, DomainTask[]>
+    fetchTasks: create.asyncThunk(
+      async (todolisrId: string, { rejectWithValue }) => {
+        try {
+          const res = await tasksApi.getTasks(todolisrId);
+          return { tasks: res.data.items, todolisrId };
+        } catch (e) {
+          return rejectWithValue(null);
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state[action.payload.todolisrId] = action.payload.tasks;
+        },
+      },
+    ),
+    createTask: create.asyncThunk(
+      async (
+        args: { todolistId: string; title: string },
+        { rejectWithValue },
+      ) => {
+        try {
+          const res = await tasksApi.createTask(args);
+          return { task: res.data.data.item };
+        } catch (error) {
+          return rejectWithValue(error);
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state[action.payload.task.todoListId].unshift(action.payload.task);
+        },
+      },
+    ),
+    deleteTask: create.asyncThunk(
+      async (
+        args: { todolistId: string; taskId: string },
+        { rejectWithValue },
+      ) => {
+        try {
+          await tasksApi.deleteTask(args);
+          return args;
+        } catch (error) {
+          return rejectWithValue(error);
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          const tasks = state[action.payload.todolistId];
+          const index = tasks.findIndex(
+            (task) => task.id === action.payload.taskId,
+          );
+          if (index !== -1) {
+            tasks.splice(index, 1);
+          }
+        },
+      },
+    ),
+  }),
+});
+
+export const { selectTasks } = tasksSlice.selectors;
+export const {
+  changeTaskStatusAC,
+  changeTaskTitleAC,
+  fetchTasks,
+  createTask,
+  deleteTask,
+} = tasksSlice.actions;
+export const tasksReducer = tasksSlice.reducer;
+
+export type TasksState = Record<string, DomainTask[]>;
--- Thunk/src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
+++ Thunk/src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
@@ -1,45 +1,58 @@
-import {EditableSpan} from "@/common/components/EditableSpan/EditableSpan"
-import {useAppDispatch} from "@/common/hooks"
-import {changeTaskStatusAC, changeTaskTitleAC, deleteTaskAC} from "@/features/todolists/model/tasks-slice"
-import DeleteIcon from "@mui/icons-material/Delete"
-import Checkbox from "@mui/material/Checkbox"
-import IconButton from "@mui/material/IconButton"
-import ListItem from "@mui/material/ListItem"
-import type {ChangeEvent} from "react"
-import {getListItemSx} from "./TaskItem.styles"
-import {DomainTask} from "@/features/todolists/api/tasksApi.types.ts";
-import {TaskStatus} from "@/common/enums";
+import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan";
+import { useAppDispatch } from "@/common/hooks";
+import {
+  changeTaskStatusAC,
+  changeTaskTitleAC,
+  deleteTask,
+} from "@/features/todolists/model/tasks-slice";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import ListItem from "@mui/material/ListItem";
+import type { ChangeEvent } from "react";
+import { getListItemSx } from "./TaskItem.styles";
+import { DomainTask } from "@/features/todolists/api/tasksApi.types.ts";
+import { TaskStatus } from "@/common/enums";
 
 type Props = {
-  task: DomainTask
-  todolistId: string
-}
+  task: DomainTask;
+  todolistId: string;
+};
 
 export const TaskItem = ({ task, todolistId }: Props) => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-  const deleteTask = () => {
-    dispatch(deleteTaskAC({ todolistId, taskId: task.id }))
-  }
+  const deleteTaskHandler = () => {
+    dispatch(deleteTask({ todolistId, taskId: task.id }));
+  };
 
   const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
-    const newStatusValue = e.currentTarget.checked
-    dispatch(changeTaskStatusAC({ todolistId, taskId: task.id, status: newStatusValue ? TaskStatus.Completed: TaskStatus.New }))
-  }
+    const newStatusValue = e.currentTarget.checked;
+    dispatch(
+      changeTaskStatusAC({
+        todolistId,
+        taskId: task.id,
+        status: newStatusValue ? TaskStatus.Completed : TaskStatus.New,
+      }),
+    );
+  };
 
   const changeTaskTitle = (title: string) => {
-    dispatch(changeTaskTitleAC({ todolistId, taskId: task.id, title }))
-  }
+    dispatch(changeTaskTitleAC({ todolistId, taskId: task.id, title }));
+  };
 
   return (
     <ListItem sx={getListItemSx(task.status === TaskStatus.Completed)}>
       <div>
-        <Checkbox checked={task.status === TaskStatus.Completed} onChange={changeTaskStatus} />
+        <Checkbox
+          checked={task.status === TaskStatus.Completed}
+          onChange={changeTaskStatus}
+        />
         <EditableSpan value={task.title} onChange={changeTaskTitle} />
       </div>
-      <IconButton onClick={deleteTask}>
+      <IconButton onClick={deleteTaskHandler}>
         <DeleteIcon />
       </IconButton>
     </ListItem>
-  )
-}
+  );
+};
