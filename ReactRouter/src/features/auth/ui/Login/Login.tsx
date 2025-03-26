b30207e9f1b451beb5783c9db93a30e8daa2e0ff
roma1524 <kulikr.js@mail.ru>
2025-03-26 21:16:34 +0300

    some refactoring

--- ReactRouter/src/features/auth/ui/Login/Login.tsx
+++ ReactRouter/src/features/auth/ui/Login/Login.tsx
@@ -1,22 +1,37 @@
 import { selectThemeMode } from "@/app/app-slice"
 import { useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
-import Button from '@mui/material/Button'
-import Checkbox from '@mui/material/Checkbox'
-import FormControl from '@mui/material/FormControl'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import FormGroup from '@mui/material/FormGroup'
-import FormLabel from '@mui/material/FormLabel'
+import Button from "@mui/material/Button"
+import Checkbox from "@mui/material/Checkbox"
+import FormControl from "@mui/material/FormControl"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import FormGroup from "@mui/material/FormGroup"
+import FormLabel from "@mui/material/FormLabel"
 import Grid from "@mui/material/Grid2"
-import TextField from '@mui/material/TextField'
+import TextField from "@mui/material/TextField"
+import { useForm } from "react-hook-form"
+
+type Inputs = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<Inputs>({ defaultValues: { email: "", password: "", rememberMe: false } })
+
   const themeMode = useAppSelector(selectThemeMode)
 
   const theme = getTheme(themeMode)
 
   return (
-    <Grid container justifyContent={'center'}>
+    <Grid container justifyContent={"center"}>
       <FormControl>
         <FormLabel>
           <p>
@@ -39,9 +54,9 @@ export const Login = () => {
           </p>
         </FormLabel>
         <FormGroup>
-          <TextField label="Email" margin="normal" />
-          <TextField type="password" label="Password" margin="normal" />
-          <FormControlLabel label="Remember me" control={<Checkbox />} />
+          <TextField label="Email" margin="normal" {...register("email")} />
+          <TextField type="password" label="Password" margin="normal" {...register("password")} />
+          <FormControlLabel label="Remember me" control={<Checkbox {...register("rememberMe")} />} />
           <Button type="submit" variant="contained" color="primary">
             Login
           </Button>
