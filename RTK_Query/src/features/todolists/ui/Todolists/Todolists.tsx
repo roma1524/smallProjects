8d0a19ab9930387b6d3e02c2c68de28ac19aaace
roma1524 <kulikr.js@mail.ru>
2025-04-09 23:00:45 +0300

    createTodolistApi

--- RTK_Query/src/features/todolists/ui/Todolists/Todolists.tsx
+++ RTK_Query/src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,22 +1,18 @@
-import { useAppDispatch, useAppSelector } from "@/common/hooks"
-import { fetchTodolistsTC, selectTodolists } from "@/features/todolists/model/todolists-slice"
+import { useAppSelector } from "@/common/hooks"
+import { selectTodolists } from "@/features/todolists/model/todolists-slice"
 import Grid from "@mui/material/Grid2"
 import Paper from "@mui/material/Paper"
-import { useEffect } from "react"
 import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { useGetTodolistsQuery } from "@/features/todolists/api/_todolistsApi.ts"
 
 export const Todolists = () => {
-  const todolists = useAppSelector(selectTodolists)
+  const res = useGetTodolistsQuery()
 
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchTodolistsTC())
-  }, [])
+  console.log(res.data)
 
   return (
     <>
-      {todolists.map((todolist) => (
+      {res.data?.map((todolist) => (
         <Grid key={todolist.id}>
           <Paper sx={{ p: "0 20px 20px 20px" }}>
             <TodolistItem todolist={todolist} />
