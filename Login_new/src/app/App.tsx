dc89a6ba4828118ff4da0648e09f55deadca4b82
roma1524 <kulikr.js@mail.ru>
2025-05-01 19:11:33 +0300

    refactoring

--- Login_new/src/app/App.tsx
+++ Login_new/src/app/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css"
+import s from './App.module.css'
 import { selectThemeMode } from "@/app/app-slice"
 import { ErrorSnackbar, Header } from "@/common/components"
 import {useAppDispatch, useAppSelector} from "@/common/hooks"
@@ -6,22 +6,35 @@ import { Routing } from "@/common/routing"
 import { getTheme } from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {meTC} from "@/features/auth/model/auth-slice.ts";
+import {CircularProgress} from "@mui/material";
 
 export const App = () => {
+  const [isInitialized, setIsInitialized] = useState(false)
+
   const themeMode = useAppSelector(selectThemeMode)
   const dispatch = useAppDispatch()
 
   const theme = getTheme(themeMode)
 
   useEffect(() => {
-    dispatch(meTC())
+    dispatch(meTC()).then(() => {
+      setIsInitialized(true)
+    })
   }, [])
+
+  if (!isInitialized) {
+    return (
+        <div className={s.circularProgressContainer}>
+          <CircularProgress size={150} thickness={3} />
+        </div>
+    )
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <div className={"app"}>
+      <div className={s.app}>
         <CssBaseline />
         <Header />
         <Routing />
