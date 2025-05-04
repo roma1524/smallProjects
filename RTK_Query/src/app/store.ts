efcb07067876475454f7701dbe8906ea21eedc62
roma1524 <kulikr.js@mail.ru>
2025-05-04 21:30:34 +0300

    added todolistApi to store

--- RTK_Query/src/app/store.ts
+++ RTK_Query/src/app/store.ts
@@ -3,6 +3,8 @@ import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice
 import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
 import { configureStore } from "@reduxjs/toolkit"
 import { appReducer, appSlice } from "./app-slice.ts"
+import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
+import { setupListeners } from "@reduxjs/toolkit/query"
 
 export const store = configureStore({
   reducer: {
@@ -10,8 +12,12 @@ export const store = configureStore({
     [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
     [authSlice.name]: authReducer,
+    [todolistsApi.reducerPath]: todolistsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todolistsApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
---
+++ RTK_Query/src/features/todolists/api/todolistsApi.ts
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { AUTH_TOKEN } from "@/common/constants"
+
+// export const _todolistsApi = {
+//   getTodolists() {
+//     return instance.get<Todolist[]>("/todo-lists")
+//   },
+//   changeTodolistTitle(payload: { id: string; title: string }) {
+//     const { id, title } = payload
+//     return instance.put<BaseResponse>(`/todo-lists/${id}`, { title })
+//   },
+//   createTodolist(title: string) {
+//     return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", { title })
+//   },
+//   deleteTodolist(id: string) {
+//     return instance.delete<BaseResponse>(`/todo-lists/${id}`)
+//   },
+// }
+
+export const todolistsApi = createApi({
+  reducerPath: "todolistsApi",
+  // `baseQuery` - конфигурация для `HTTP-клиента`, который будет использоваться для отправки запросов
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    prepareHeaders: (headers) => {
+      headers.set("API-KEY", import.meta.env.VITE_API_KEY)
+      headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+    },
+  }),
+  endpoints: (build) => ({
+    getTodolists: build.query<any[], void>({
+      query: () => "todo-lists",
+    }),
+  }),
+})
+
+export const { useGetTodolistsQuery } = todolistsApi
