f9997856f13ccd78074df46524a743b81a2e7035
roma1524 <kulikr.js@mail.ru>
2025-05-04 22:17:28 +0300

    fetching data with useGetTodolistsQuery

--- RTK_Query/src/features/todolists/api/todolistsApi.ts
+++ RTK_Query/src/features/todolists/api/todolistsApi.ts
@@ -1,25 +1,28 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { AUTH_TOKEN } from "@/common/constants"
+import { instance } from "@/common/instance"
+import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts"
+import { BaseResponse } from "@/common/types"
+import { DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
 
-// export const _todolistsApi = {
-//   getTodolists() {
-//     return instance.get<Todolist[]>("/todo-lists")
-//   },
-//   changeTodolistTitle(payload: { id: string; title: string }) {
-//     const { id, title } = payload
-//     return instance.put<BaseResponse>(`/todo-lists/${id}`, { title })
-//   },
-//   createTodolist(title: string) {
-//     return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", { title })
-//   },
-//   deleteTodolist(id: string) {
-//     return instance.delete<BaseResponse>(`/todo-lists/${id}`)
-//   },
-// }
+export const _todolistsApi = {
+  getTodolists() {
+    return instance.get<Todolist[]>("/todo-lists")
+  },
+  changeTodolistTitle(payload: { id: string; title: string }) {
+    const { id, title } = payload
+    return instance.put<BaseResponse>(`/todo-lists/${id}`, { title })
+  },
+  createTodolist(title: string) {
+    return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", { title })
+  },
+  deleteTodolist(id: string) {
+    return instance.delete<BaseResponse>(`/todo-lists/${id}`)
+  },
+}
 
 export const todolistsApi = createApi({
   reducerPath: "todolistsApi",
-  // `baseQuery` - конфигурация для `HTTP-клиента`, который будет использоваться для отправки запросов
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_BASE_URL,
     prepareHeaders: (headers) => {
@@ -28,8 +31,10 @@ export const todolistsApi = createApi({
     },
   }),
   endpoints: (build) => ({
-    getTodolists: build.query<any[], void>({
+    getTodolists: build.query<DomainTodolist[], void>({
       query: () => "todo-lists",
+      transformResponse: (todoLists: Todolist[]): DomainTodolist[] =>
+        todoLists.map((t) => ({ ...t, filter: "all", entityStatus: "idle" })),
     }),
   }),
 })
--- RTK_Query/src/features/todolists/ui/Todolists/Todolists.tsx
+++ RTK_Query/src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,22 +1,16 @@
-import { useAppDispatch, useAppSelector } from "@/common/hooks"
-import { fetchTodolistsTC, selectTodolists } from "@/features/todolists/model/todolists-slice"
 import Grid from "@mui/material/Grid2"
 import Paper from "@mui/material/Paper"
-import { useEffect } from "react"
 import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { useGetTodolistsQuery } from "@/features/todolists/api/todolistsApi.ts"
 
 export const Todolists = () => {
-  const todolists = useAppSelector(selectTodolists)
+  const { data: todoLists } = useGetTodolistsQuery()
 
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchTodolistsTC())
-  }, [])
+  console.log(todoLists)
 
   return (
     <>
-      {todolists.map((todolist) => (
+      {todoLists?.map((todolist) => (
         <Grid key={todolist.id}>
           <Paper sx={{ p: "0 20px 20px 20px" }}>
             <TodolistItem todolist={todolist} />
