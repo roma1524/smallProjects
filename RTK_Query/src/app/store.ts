7995bd1c4dd34459d8ce9a53526ceebb8b132e6d
roma1524 <kulikr.js@mail.ru>
2025-05-06 15:45:05 +0300

    added baseApi

--- RTK_Query/src/app/store.ts
+++ RTK_Query/src/app/store.ts
@@ -3,8 +3,8 @@ import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice
 import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
 import { configureStore } from "@reduxjs/toolkit"
 import { appReducer, appSlice } from "./app-slice.ts"
-import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
 import { setupListeners } from "@reduxjs/toolkit/query"
+import { baseApi } from "@/app/baseApi.ts"
 
 export const store = configureStore({
   reducer: {
@@ -12,9 +12,9 @@ export const store = configureStore({
     [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
     [authSlice.name]: authReducer,
-    [todolistsApi.reducerPath]: todolistsApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todolistsApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 setupListeners(store.dispatch)
