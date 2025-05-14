61951d873da53573b16388a5088f0cdafcb9358a
roma1524 <kulikr.js@mail.ru>
2025-05-14 20:07:39 +0300

    create changeTodolistStatus function

--- Loaders_Errors/src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ Loaders_Errors/src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -1,9 +1,15 @@
 import { EditableSpan } from "@/common/components"
-import { useRemoveTodolistMutation, useUpdateTodolistTitleMutation } from "@/features/todolists/api/todolistsApi"
+import {
+  todolistsApi,
+  useRemoveTodolistMutation,
+  useUpdateTodolistTitleMutation,
+} from "@/features/todolists/api/todolistsApi"
 import { type DomainTodolist } from "@/features/todolists/model/todolists-slice"
 import DeleteIcon from "@mui/icons-material/Delete"
 import IconButton from "@mui/material/IconButton"
 import styles from "./TodolistTitle.module.css"
+import { useAppDispatch } from "@/common/hooks"
+import { RequestStatus } from "@/common/types"
 
 type Props = {
   todolist: DomainTodolist
@@ -12,11 +18,29 @@ type Props = {
 export const TodolistTitle = ({ todolist }: Props) => {
   const { id, title, entityStatus } = todolist
 
+  const dispatch = useAppDispatch()
+
   const [removeTodolist] = useRemoveTodolistMutation()
   const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
 
   const deleteTodolist = () => {
+    changeTodolistStatus("loading")
     removeTodolist(id)
+      .unwrap()
+      .catch(() => {
+        changeTodolistStatus("failed")
+      })
+  }
+
+  const changeTodolistStatus = (entityStatus: RequestStatus) => {
+    dispatch(
+      todolistsApi.util.updateQueryData("getTodolists", undefined, (state) => {
+        const todolist = state.find((todolist) => todolist.id === id)
+        if (todolist) {
+          todolist.entityStatus = entityStatus
+        }
+      }),
+    )
   }
 
   const changeTodolistTitle = (title: string) => {
