8c699ea77f09b612bf572a758cfeb8bbefdad96e
roma1524 <kulikr.js@mail.ru>
2025-05-19 22:37:22 +0300

    catch tags

---
+++ Catch tags/src/features/auth/api/authApi.ts
@@ -0,0 +1,26 @@
+import { baseApi } from "@/app/baseApi"
+import type { BaseResponse } from "@/common/types"
+import type { LoginArgs } from "./authApi.types"
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    me: build.query<BaseResponse<{ id: number; email: string; login: string }>, void>({
+      query: () => "auth/me",
+    }),
+    login: build.mutation<BaseResponse<{ userId: number; token: string }>, LoginArgs>({
+      query: (body) => ({
+        url: "auth/login",
+        method: "POST",
+        body,
+      }),
+    }),
+    logout: build.mutation<BaseResponse, void>({
+      query: () => ({
+        url: "auth/login",
+        method: "DELETE",
+      }),
+    }),
+  }),
+})
+
+export const { useMeQuery, useLoginMutation, useLogoutMutation } = authApi
