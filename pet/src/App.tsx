129063cca05e23ba062d3bb66fb6d9e8c3ca8847
roma1524 <kulikr.js@mail.ru>
2025-06-03 13:49:02 +0300

    create Todolist

--- pet/src/App.tsx
+++ pet/src/App.tsx
@@ -1,136 +1,27 @@
 import './App.css';
-import {TaskList} from "./TaskList.tsx";
+import {Todolist} from "./Todolist.tsx";
+import {useState} from "react";
 
-type TaskType = {
-    taskId: number
-    title: string
-    isDone: boolean
-}
-
-export type DataType = {
-    title: string
-    tasks: TaskType[]
-    students: string[]
-}
+export type FilterValuesType = "all" | "active" | "completed";
 
 export const App = ()=> {
-    const data1: DataType = {
-        title: "What to do",
-        tasks: [
-            {taskId: 1, title: "HTML&CSS2", isDone: true},
-            {taskId: 2, title: "JS2", isDone: true}
-        ],
-        students: [
-            'Jago Wormald1',
-            'Saul Milne2',
-            'Aariz Hester3',
-            'Dion Reeve4',
-            'Anisa Ortega5',
-            'Blade Cisneros6',
-            'Malaikah Phelps7',
-            'Zeeshan Gallagher8',
-            'Isobella Vo9',
-            'Rizwan Mathis10',
-            'Menaal Leach11',
-            'Kian Walton12',
-            'Orion Lamb13',
-            'Faizah Huynh14',
-            'Crystal Vaughan15',
-            'Vivien Hickman16',
-            'Stuart Lu17',
-            'Karol Davison18',
-            'Dario Burns19',
-            'Chloe Rich20',
-            'Martyna Felix',
-            'Nida Glass',
-            'Maeve Miles',
-            'Hasnain Puckett',
-            'Ayman Cano',
-            'Safwan Perry',
-            'Fox Kelly',
-            'Louise Barlow',
-            'Malaki Mcgill',
-            'Leanna Cline',
-            'Willard Hodge',
-            'Amelia Dorsey',
-            'Kiah Porter',
-            'Jeanne Daly',
-            'Mohsin Armstrong',
-            'Laurie Rangel',
-            'Princess Tierney',
-            'Kasim Kendall',
-            'Darryl Cope',
-            'Elysha Ray',
-            'Liyana Harris',
-            'Kashif Blackburn',
-            'Atif Zimmerman',
-            'Sila Hartley',
-            'Ralphie Hebert',
-        ]
-    }
-    const data2 : DataType =   {
-        title: "What to learn",
-        tasks: [
-            {taskId: 1, title: "HTML&CSS", isDone: true},
-            {taskId: 2, title: "JS", isDone: true}
-        ],
-        students: [
-            'Rick Kane',
-            'Finnlay Bentley',
-            'Samia North',
-            'Isaac Morton',
-            'Lily-Ann Clifford',
-            'Thalia Park',
-            'Sapphire Cruz',
-            'Cieran Vazquez',
-            'Anya Estes',
-            'Dominika Field',
-            'Rosanna Chung',
-            'Safiyah Davey',
-            'Ryley Beasley',
-            'Kalvin Trejo',
-            'Evie-Mae Farrell',
-            'Juliet Valencia',
-            'Astrid Austin',
-            'Lyle Montgomery',
-            'Nisha Mora',
-            'Kylie Callaghan',
-            'Star Wilks',
-            'Marissa Colley',
-            'Asa Fuller',
-            'Leigh Kemp',
-            'Avleen Dawson',
-            'Sammy Bonilla',
-            'Acacia Becker',
-            'Coral Shepherd',
-            'Melina Molina',
-            'Kiran Bailey',
-            'Clara Escobar',
-            'Alexandru Horn',
-            'Brandon-Lee Mercado',
-            'Elouise Weston',
-            'King Long',
-            'Kerri Searle',
-            'Kanye Hamer',
-            'Elwood Benitez',
-            'Mikail Whitaker',
-            'Bobby Hardy',
-            'Talha Ferry',
-            'Priscilla Landry',
-            'Olivia-Grace Cain',
-            'Kiaan Wallace',
-            'Wesley Padilla90',
-            'Ella-Grace Wooten91',
-            'Kaif Molloy92',
-            'Kamal Broadhurst93',
-            'Bianca Ferrell94',
-            'Micheal Talbot95',
-        ]
-    }
+    const [tasks, setTasks] = useState([
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Rest API", isDone: false},
+        {id: 5, title: "GraphQL", isDone: false},
+    ]);
+
+    const [filter, setFilter] = useState<FilterValuesType>("all");
+
 
     return (
         <div className="App">
-            <TaskList data={data2} />
+            <Todolist title="What to learn"
+                      tasks={tasksForTodolist}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}/>
         </div>
     );
 }
