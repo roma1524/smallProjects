836a697fea3c8510b8525385542a1b0a93dbcdb9
roma1524 <kulikr.js@mail.ru>
2025-06-13 14:22:26 +0300

    refactoring socket project

--- socket/src/App.tsx
+++ socket/src/App.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useRef, useState} from 'react'
 import './App.css'
 import {io} from "socket.io-client";
+import {v1} from "uuid";
 
 type Message = {
     message: string;
@@ -15,32 +16,65 @@ const socket = io('http://localhost:5115');
 
 function App() {
 
-    useEffect(() => {
-        socket.on('init-mess', (messages: Message[]) => {
-            setMessages(messages)
-        })
-        socket.on('new-message-sent', (messageItem: Message) => {
-            setMessages(prevState => [...prevState, messageItem])
-        })
-    }, [])
-
     const [messages, setMessages] = useState<Message[]>([])
-    const [message, setMessage] = useState('Hello')
+    const [message, setMessage] = useState('')
     const [name, setName] = useState('')
+    const messagesAnchorRef = useRef<HTMLDivElement>(null)
+    const [autoScrollActive, setAutoScrollActive] = useState(true)
+
+    useEffect(() => {
+        const handleInitMessages = (messages: Message[]) => {
+            setMessages(messages);
+        };
+
+        const handleNewMessage = (messageItem: Message) => {
+            setMessages(prevState => [...prevState, messageItem]);
+        };
+
+        socket.on('init-mess', handleInitMessages);
+        socket.on('new-message-sent', handleNewMessage);
+
+        return () => {
+            socket.off('init-mess', handleInitMessages);
+            socket.off('new-message-sent', handleNewMessage);
+        };
+
+        // socket.on('init-mess', (messages: Message[]) => {
+        //     setMessages(messages)
+        // })
+        // socket.on('new-message-sent', (messageItem: Message) => {
+        //     setMessages(prevState => [...prevState, messageItem])
+        // })
+    }, [])
 
     useEffect(() => {
         messagesAnchorRef.current?.scrollIntoView({behavior: 'smooth'})
     }, [messages]);
 
-    const messagesAnchorRef = useRef<HTMLDivElement>(null)
+    const handleSendMessage = () => {
+        if (message.trim()) {
+            socket.emit('client-message-sent', message.trim());
+            setMessage('');
+        }
+    };
+
+    const handleSetName = () => {
+        if (name.trim()) {
+            socket.emit('client-name', name.trim());
+        }
+    };
 
     return (
         <div className='App'>
             <div>
-                <div style={{border: '1px solid black', padding: '10px', height: '300px', overflowY: 'scroll'}}>
+                <div style={{border: '1px solid black', padding: '10px', height: '300px', overflowY: 'scroll'}}
+                onScroll={() => {
+                    
+                }}
+                >
                     {messages?.map((item) => {
                         return (
-                            <div key={item.id}>
+                            <div key={v1()}>
                                 <b>{item.user.name}:</b> {item.message}
                                 <hr/>
                             </div>
@@ -49,15 +83,14 @@ function App() {
                     <div ref={messagesAnchorRef}></div>
                 </div>
                 <input value={name} onChange={(e) => setName(e.currentTarget.value)}/>
-                <button onClick={() => {
-                    socket.emit('client-name', name)
-                }}>Send Name
+                <button onClick={handleSetName}>Send Name
                 </button>
-                <textarea value={message} onChange={(e) => setMessage(e.currentTarget.value)}></textarea>
-                <button onClick={() => {
-                    socket.emit('client-message-sent', message)
-                    setMessage('')
-                }}>Send
+                <textarea value={message} onChange={
+                    (e) => setMessage(e.currentTarget.value)
+                }
+                placeholder='Message'
+                ></textarea>
+                <button onClick={handleSendMessage}>Send
                 </button>
             </div>
         </div>
