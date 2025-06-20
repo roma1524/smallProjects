b36e12d3a70a6d9b91ee31b4dd059d193d9b072f
roma1524 <kulikr.js@mail.ru>
2025-06-20 23:13:03 +0300

    create node.js project

---
+++ Back-End/src/index.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from 'express'
+const app = express()
+const port = 3001
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World! 777')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
