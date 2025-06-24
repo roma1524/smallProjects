15a44305e60b9a42d8cd0a84184e9b5c0ef2c015
roma1524 <kulikr.js@mail.ru>
2025-06-24 23:15:53 +0300

    practice docker

--- docker_app/index.js
+++ docker_app/index.js
@@ -1,2 +1,8 @@
-
-console.log('Hello world');
+let count = 0;
+const intervalId = setInterval(() => {
+    console.log('Строка: ' + count);
+    count++;
+    if (count >= 100) {
+        clearInterval(intervalId);  // Останавливаем, когда достигли 100
+    }
+}, 1000);
